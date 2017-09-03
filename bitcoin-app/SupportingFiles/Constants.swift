f4228a10c0a13d48821e1015ea591708092353cf
Jan Damm <jandammshaeuser@gmail.com>
2017-09-03 12:31:08 +0200

    Add bitcoin api urls

---
+++ bitcoin-app/SupportingFiles/Constants.swift
@@ -0,0 +1,38 @@
+//
+//  Constants.swift
+//  bitcoin-app
+//
+//  Created by Jan Dammshäuser on 03.09.17.
+//  Copyright © 2017 Jan Dammshäuser. All rights reserved.
+//
+
+import Foundation
+
+enum Api {
+    
+    static let refreshInterval: TimeInterval = 15
+    
+    enum Url {
+        private static let baseUrl = "https://apiv2.bitcoinaverage.com/indices/global/"
+        private static let tickerUrl = "ticker/"
+        private static let historyUrl = "history/"
+        
+        static func forTicker(with conversion: BitcoinConversion) -> URL {
+            return getURL(path: tickerUrl, conversion: conversion)
+        }
+        
+        static func forHistory(with conversion: BitcoinConversion) -> URL {
+            return getURL(path: historyUrl, conversion: conversion)
+        }
+        
+        private static func getURL(path: String, conversion: BitcoinConversion) -> URL {
+            let fullPath = "\(baseUrl)\(path)\(conversion.symbol)"
+            
+            guard let url = URL(string: fullPath) else {
+                fatalError("The given Strings should always generate a valid URL")
+            }
+            
+            return url
+        }
+    }
+}
