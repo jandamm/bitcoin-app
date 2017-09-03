3f6f3e5dc5a936efe7fbab9ce9c0b11e4c9f5106
Jan Damm <jandammshaeuser@gmail.com>
2017-09-03 11:28:00 +0200

    Add gitignore, cocoapods, AF, JDCoordinator, Charts

---
+++ bitcoin-app/AppCoordinator.swift
@@ -0,0 +1,11 @@
+//
+//  AppCoordinator.swift
+//  bitcoin-app
+//
+//  Created by Jan Dammshäuser on 03.09.17.
+//  Copyright © 2017 Jan Dammshäuser. All rights reserved.
+//
+
+import JDCoordinator
+
+class AppCoordinator: JDAppCoordinator {}
